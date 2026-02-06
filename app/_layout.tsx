20ecb1dd4d091fb9f1c5c9258abbc40c177aee56
Anurag <anuraggoutam01@gmail.com>
2026-02-06 09:04:01 +0530

    first commit

---
+++ app/_layout.tsx
@@ -0,0 +1,39 @@
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import React, { useEffect } from 'react';
+import '../global.css';
+
+SplashScreen.preventAutoHideAsync();
+
+const RootLayout = () => {
+  const [loaded] = useFonts({
+    'Jakarta-Bold': require('../assets/fonts/PlusJakartaSans-Bold.ttf'),
+    'Jakarta-ExtraBold': require('../assets/fonts/PlusJakartaSans-ExtraBold.ttf'),
+    'Jakarta-ExtraLight': require('../assets/fonts/PlusJakartaSans-ExtraLight.ttf'),
+    'Jakarta-Light': require('../assets/fonts/PlusJakartaSans-Light.ttf'),
+    'Jakarta-Medium': require('../assets/fonts/PlusJakartaSans-Medium.ttf'),
+    Jakarta: require('../assets/fonts/PlusJakartaSans-Regular.ttf'),
+    'Jakarta-SemiBold': require('../assets/fonts/PlusJakartaSans-SemiBold.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" options={{ title: 'Home' }} />
+      <Stack.Screen name="(auth)" />
+      <Stack.Screen name="(root)" />
+      <Stack.Screen name="not-found" options={{ title: 'Not Found' }} />
+    </Stack>
+  );
+};
+
+export default RootLayout;
---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const index = () => {
+  return (
+    <View className="flex-1 bg-slate-400  justify-center items-center">
+      <Text className="color-red-500">index</Text>
+    </View>
+  );
+};
+
+export default index;
